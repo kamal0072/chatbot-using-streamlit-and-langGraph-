77b45fe60856621275bb0e5ddae00b1ae9733a28
Haaris <haaris@trainingbasket.in>
2026-01-12 17:54:43 +0530

    This is a new commit for what I originally planned to be amended

--- messages.py
+++ messages.py
@@ -1,77 +1,16 @@
-# from langchain_huggingface import HuggingFaceEndpointEmbeddings
-# from langchain_classic.schema import Document
-# from langchain_classic.retrievers.contextual_compression import ContextualCompressionRetriever
-# from langchain_classic.retrievers.document_compressors import LLMChainExtractor
-# from langchain_core.documents import Document
-
-# from langchain_core.output_parsers import StrOutputParser
-# from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
-# from langchain_core.prompts import PromptTemplate
-# from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace, HuggingFaceEndpointEmbeddings
-# from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound
-# from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
-# from langchain_community.vectorstores import FAISS
-# try:
-#     video_id = "Gfr50f6ZBvo"    # only the ID, not full URL/transcript not found for this will show error
-#     # video_id = "Gfr50f6ZBvo"
-#     # If you don’t care which language, this returns the “best” one
-#     api = YouTubeTranscriptApi()
-#     transcript = api.fetch(video_id=video_id, languages=['en', 'hi'])
-#     transcript_list = transcript.snippets
-#     transcript_list = [transcript_list[i].text for i in range(len(transcript_list))]
-#     lis = " ".join(transcript_list)
-
-#     splitter =  RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
-#     chunks = splitter.create_documents([lis])
-#     # print(chunks[100])
-#     # print(len(chunks))
-#     embeddings = HuggingFaceEndpointEmbeddings( )
-#     vector_store = FAISS.from_documents(
-#         documents=chunks,
-#         embedding=embeddings
-#     )
-#     # print(vector_store.index_to_docstore_id)
-#     # print(vector_store.get_by_ids(['3d495d0a-3f66-4742-86f7-626845e22f7e']))
-
-#     llm=HuggingFaceEndpoint(
-#         repo_id="meta-llama/Llama-3.2-3B-Instruct",
-#         task="text-generation",
-#         max_new_tokens=100,
-#         temperature=0.3
-#     )
-#     prompt = PromptTemplate(
-#         template="""      You are a helpful assistant.
-#       Answer ONLY from the provided transcript context.
-#       If the context is insufficient, just say you don't know.
-
-#     {context}
-#     Question: {question}""",
-#     input_variables=["context", "question"]
-#     )
-#     retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 2})
-#     question = "What is the meaning of life?"
-
-#     retrieved_docs = retriever.invoke(question)
-
-#     def format_docs(retrieved_docs):
-#         context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
-#         return context_text
-#     parallel_chain = RunnableParallel({
-#         'context': retriever | RunnableLambda(format_docs),
-#         'question': RunnablePassthrough()
-#     })
-#     parser = StrOutputParser()
-#     main_chain = parallel_chain | prompt | llm | parser
-#     print(main_chain.invoke("can you summary this video in 2-3 sentences?"))
-
-
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
-from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
+import dotenv
+import os
 
+dotenv.load_dotenv()
+# checking langghain api key
+# print(os.getenv("LANGCHAIN_API_KEY"))
 # make llm
 model = ChatHuggingFace(
     llm=HuggingFaceEndpoint(
@@ -92,8 +31,11 @@ def chat_node(state: ChatState) -> ChatState:
     response = model.invoke(messages)
     return {'messages' : [response]}
 
+# create database
+conn = sqlite3.connect(database='chatbotData.db', check_same_thread=False) #
 # checkpointer
-checkpointer = InMemorySaver()
+# checkpointer = InMemorySaver()
+checkpointer = SqliteSaver(conn=conn)
 
 # creating graph
 graph = StateGraph(ChatState)
@@ -104,3 +46,15 @@ graph.add_edge('chat_node', END)
 
 # chatbot
 chatbot = graph.compile(checkpointer = checkpointer)
+
+# checkpointer = checkpointer.list()
+# print("checkpointer generater :- ", checkpointer)
+# print(len(checkpointer))
+def retrive_all_thread():
+    all_set = set()
+    for checkpoint in checkpointer.list(None):
+        # print(checkpoint)
+        # print(checkpoint.config)
+        # print(checkpoint.config["configurable"]["thread_id"])
+        all_set.add(checkpoint.config["configurable"]["thread_id"])
+    return list(all_set)
--- streamlit_client.py
+++ streamlit_client.py
@@ -1,13 +1,13 @@
 import streamlit as st
-from messages import chatbot
+from langGraph_tools import chatbot, retrieve_all_threads
 from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
 import uuid
 
 # **************************************** utility functions *************************
-
 def generate_thread_id():
     thread_id = uuid.uuid4()
     return thread_id
+
 
 def reset_chat():
     thread_id = generate_thread_id()
@@ -15,14 +15,18 @@ def reset_chat():
     add_thread(st.session_state['thread_id'])
     st.session_state['message_history'] = []
 
+
 def add_thread(thread_id):
     if thread_id not in st.session_state['chat_threads']:
         st.session_state['chat_threads'].append(thread_id)
 
+
 def load_conversation(thread_id):
-    state = chatbot.get_state(config={'configurable': {'thread_id': thread_id}})
+    state = chatbot.get_state(
+        config={'configurable': {'thread_id': thread_id}})
     # Check if messages key exists in state values, return empty list if not
     return state.values.get('messages', [])
+
 
 st.set_page_config(
     page_title="Chatbot for a change",
@@ -36,10 +40,10 @@ if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
 if 'thread_id' not in st.session_state:
-    st.session_state['thread_id'] = generate_thread_id()    
+    st.session_state['thread_id'] = generate_thread_id()
 
 if 'chat_threads' not in st.session_state:
-    st.session_state['chat_threads'] = []
+    st.session_state['chat_threads'] = retrieve_all_threads()
 add_thread(st.session_state['thread_id'])
 
 
@@ -48,7 +52,7 @@ st.sidebar.title("Chatbot for a change")
 if st.sidebar.button("New Chat Session"):
     reset_chat()
 st.sidebar.header("My chat history")
-    
+
 for thread_id in st.session_state['chat_threads'][::-1]:
     if st.sidebar.button(str(thread_id)):
         print("The thread id is + ", thread_id)
@@ -59,9 +63,9 @@ for thread_id in st.session_state['chat_threads'][::-1]:
 
         for msg in messages:
             if isinstance(msg, HumanMessage):
-                role='user'
+                role = 'user'
             else:
-                role='assistant'
+                role = 'assistant'
             temp_messages.append({'role': role, 'content': msg.content})
 
         st.session_state['message_history'] = temp_messages
@@ -76,11 +80,20 @@ for message in st.session_state['message_history']:
 user_input = st.chat_input(placeholder="Type your message here...")
 if user_input:
     # first add the messags to messages history
-    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    st.session_state['message_history'].append(
+        {'role': 'user', 'content': user_input})
     with st.chat_message('user', avatar="https://i.pravatar.cc/150?u=a042581f4e28540bc8c1"):
         st.text(user_input)
 
-    CONFIG = {"configurable" : {'thread_id' : st.session_state['thread_id']}}
+    # CONFIG = {"configurable" : {'thread_id' : st.session_state['thread_id']}}
+
+    CONFIG = {
+        "configurable": {'thread_id': st.session_state['thread_id']},
+        "metadata" : {
+            'thread_id' : st.session_state['thread_id']
+        },
+        "run_name" : "Chatbot With UI"
+    }
 
     # first add the message to message_history
     with st.chat_message('assistant', avatar="https://i.pravatar.cc/150?u=a042581f4e28540bc8c2"):
@@ -94,4 +107,5 @@ if user_input:
                     # yield only assistant tokens
                     yield message_chunk.content
         ai_message = st.write_stream(ai_only_stream)
-    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message}, )
+    st.session_state['message_history'].append(
+        {'role': 'assistant', 'content': ai_message}, )
